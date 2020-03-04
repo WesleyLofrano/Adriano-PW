9800671faa78f98352c378ce059e70999ebc18ea
Wesley- <lofrano.wesley@hotmail.com>
2020-03-04 20:45:30 -0300

    Atualização

--- js/Wesley-.js
+++ js/Wesley-.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
     $('#Telefone').mask('(99) 9999-9999')
     $('#Celular').mask('(99) 9999-9999')
+    $('#Data').mask('99/99/9999')
+    $('#CPF').mask('999.999.999-99')
 })
